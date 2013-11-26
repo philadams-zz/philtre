a458b95ab48b8724660bc0174244342d63dd7a22
Phil Adams <philadams.net@gmail.com>
2013-11-26 13:13:33 -0500

    add flipHorizontal filter

--- js/app.js
+++ js/app.js
@@ -8,13 +8,15 @@ var idata = Philtre.filterImage(Philtre.brighten, img, 40);
 var idata = Philtre.filterImage(Philtre.contrast, img, 25);
 var idata = Philtre.filterImage(Philtre.saturate, img, 40);
 var idata = Philtre.filterImage(Philtre.sepia, img, 40);
-var idata = Philtre.filterImage(Philtre.flipHorizontal, img);
 var idata = Philtre.filterImage(Philtre.flipVertical, img);
 var idata = Philtre.filterImage(Philtre.invert, img);
 var idata = Philtre.filterImage(Philtre.threshold, img, 124);
+var idata = Philtre.filterImage(Philtre.flipHorizontal, img);
 
 // show results
-var canvas = Philtre.getCanvas(img.width, img.height);
+var canvas = document.createElement('canvas');
+canvas.width = img.width;
+canvas.height = img.height;
 var ctx = canvas.getContext('2d');
 ctx.putImageData(idata, 0, 0);
 img.parentNode.insertBefore(canvas, img.nextSibling);
--- js/philtre.js
+++ js/philtre.js
@@ -1,6 +1,10 @@
 Philtre = {};
 
-// img from document.getElement
+// a canvas for all not-in-place pixel wrangling
+Philtre.scratchCanvas = document.createElement('canvas');
+Philtre.scratchCtx = Philtre.scratchCanvas.getContext('2d');
+
+// imagedata of the img from document.getElement
 Philtre.getPixels = function(img) {
   var canvas = this.getCanvas(img.width, img.height);
   var ctx = canvas.getContext('2d');
@@ -8,11 +12,18 @@ Philtre.getPixels = function(img) {
   return ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
+// create a canvas with the specified width and height
 Philtre.getCanvas = function(width, height) {
-  canvas = document.createElement('canvas');
+  var canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
   return canvas;
+};
+
+// empty imagedata object for filtered output
+// either from a tmpCanvas.ctx, or (for signed data) a Float32Array
+Philtre.createImageData = function(width, height) {
+  return this.scratchCtx.createImageData(width, height);
 };
 
 Philtre.filterImage = function(filter, image, var_args) {
@@ -35,22 +46,36 @@ Philtre.brighten = function(pixels, adjustment) {
     d[i+2] += adjustment;
   }
   return pixels;
-}
+};
 
 Philtre.contrast = function(pixels, percent) {
   console.error('Not yet implemented');
   return pixels;
-}
+};
 
 Philtre.flipHorizontal = function(pixels) {
-  console.error('Not yet implemented');
-  return pixels;
-}
+  var src = pixels.data,
+      w = pixels.width,
+      h = pixels.height;
+  var output = Philtre.createImageData(w, h);
+  var dst = output.data;
+  for (var y=0; y<h; y++) {
+    for (var x=0; x<w; x++) {
+      var srcpxl = (y*w+x)*4;
+      var dstpxl = (y*w+(w-x-1))*4;
+      dst[dstpxl] = src[srcpxl];
+      dst[dstpxl+1] = src[srcpxl+1];
+      dst[dstpxl+2] = src[srcpxl+2];
+      dst[dstpxl+3] = src[srcpxl+3];
+    }
+  }
+  return output;
+};
 
 Philtre.flipVertical = function(pixels) {
   console.error('Not yet implemented');
   return pixels;
-}
+};
 
 Philtre.grayscale = function(pixels, args) {
   var d = pixels.data;
@@ -75,7 +100,7 @@ Philtre.identity = function(pixels, args) {
     //d[i+3] = d[i+3];  // optional alpha
   }
   return pixels;
-}
+};
 
 Philtre.invert = function(pixels) {
   var d = pixels.data;
@@ -86,17 +111,17 @@ Philtre.invert = function(pixels) {
     //d[i+3] = 255 - d[i+3];  // optional alpha
   }
   return pixels;
-}
+};
 
 Philtre.saturate = function(pixels, percent) {
   console.error('Not yet implemented');
   return pixels;
-}
+};
 
 Philtre.sepia = function(pixels, percent) {
   console.error('Not yet implemented');
   return pixels;
-}
+};
 
 Philtre.threshold = function(pixels, threshold) {
   // 0.299*R + 0.587*G + 0.114*B
@@ -114,4 +139,4 @@ Philtre.threshold = function(pixels, threshold) {
     d[i] = d[i+1] = d[i+2] = v;
   }
   return pixels;
-}
+};
